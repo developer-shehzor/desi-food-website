0312c25e4a6532bd2f2809b9faa0f7df7db42421
ShehzorMemon <Shezymemon0180@gmail.com>
2023-07-06 15:13:19 +0500

    Done with adding all the files from GitHub Desktop.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/basics/Navbar'
+import Restaurant from './components/basics/Restaurant'
+
+
+function App() {
+  return (
+    <>
+      <Restaurant />
+    </>
+  )
+}
+
+export default App
+
---
+++ src/components/basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "At our restaurant, we proudly serve Maggi dishes that cater to those who crave its delightful taste and convenience. Our menu features a variety of Maggi options, ranging from classic Maggi noodles with a choice of flavorful sauces..",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "At our restaurant, we take pride in serving delicious aalu pakodas that are crispy on the outside and soft on the inside. Our skilled chefs carefully prepare..",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "At our restaurant, we celebrate the natural sweetness and vibrant flavors of corn in our dishes. We source the freshest corn and incorporate it into a range of delectable preparations..",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "At our restaurant, we take pride in preparing delicious Chola that showcases the authentic taste of this classic dish. Our chefs start by simmering..",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "From classic favorites like Margherita, topped with fresh basil and mozzarella, to unique creations featuring gourmet ingredients like prosciutto, arugula, or truffle oil..",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "At our restaurant, we take pride in curating a Non-Veg Thali that offers a diverse range of flavors and textures. Our chefs carefully prepare a combination of delectable..",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "At our restaurant, we offer a delectable selection of sweets that cater to different tastes and preferences. From traditional classics to innovative..",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "At our restaurant, we take pride in preparing delicious Rajma rice that highlights the authentic taste of this traditional dish..",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "At our restaurant, we pride ourselves on serving mouthwatering samosas that are crispy on the outside and bursting with flavorful..",
+    },
+  ];
+  
+  export default Menu;
